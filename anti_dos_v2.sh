34b943e2f354b47842e95b6e2bbd1fa942c53923
Abdurrahman Eshibiny <a.eshibiny@gmail.com>
2022-02-08 22:37:57 +0200

    uploading semi-final version of the script

---
+++ anti_dos_v2.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+sudo apt install tcpdump
+sudo apt install iptables
+
+
+declare -A ipTable
+
+function check_history {
+	for i in ${!ipTable[*]}
+	do
+		if [ "$1" = "$i" ] 
+		then
+			#echo "IP not new"
+			res=$(expr ${ipTable[$i]} + 1)
+			unset ipTable[$i]
+			ipTable+=([$i]=$res)
+			if [ ${ipTable[$i]} -gt 100 ]
+			then
+				#echo "DDoS attack detected..."
+				#ip=`echo $most | awk '{print $2}'`
+				sudo iptables -A INPUT -s $i -j DROP
+			fi
+			return 1
+		fi
+	done
+	#echo "New IP"
+	ipTable+=([$1]=1)
+}
+
+while read line
+do 
+	if [ $SECONDS -gt 1 ]
+	then
+		unset ipTable
+		declare -A ipTable
+		sudo iptables -F
+		SECONDS=0
+	fi
+	if [ ${ipTable[$line]} -gt 100 ]
+	then
+		continue
+	fi
+	check_history $line
+	echo "IP: $line , No. of sent requests: ${ipTable[$line]}"
+done < <(sudo tcpdump  'icmp[icmptype] == icmp-echo' -tnl | awk '{print $2}')
