886a605841ff5e127ef49719db8737741eb31298
Abdurrahman Eshibiny <a.eshibiny@gmail.com>
2022-02-16 04:49:18 +0200

    added support for tcp-syn attacks and fixed bugs

--- anti_dos_v3.sh
+++ anti_dos_v3.sh
@@ -8,13 +8,14 @@ declare -A ipTable
 function check_history {
 	for i in ${!ipTable[*]}
 	do
+		#echo $i
 		if [ "$1" = "$i" ] 
 		then
 			#echo "IP not new"
 			res=$(expr ${ipTable[$i]} + 1)
 			unset ipTable[$i]
 			ipTable+=([$i]=$res)
-			if [ ${ipTable[$i]} -gt 100 ]
+			if [ ${ipTable[$i]} -eq 100 ]
 			then
 				#echo "DDoS attack detected..."
 				#ip=`echo $most | awk '{print $2}'`
@@ -29,17 +30,22 @@ function check_history {
 
 while read line
 do 
+	#echo $line
+	ip=$( echo $line | awk '{print $2}')
+	ip="$(echo $ip | cut -d . -f 1).$(echo $ip | cut -d . -f 2).$(echo $ip | cut -d . -f 3).$(echo $ip | cut -d . -f 4)"
+	echo $ip
+	check_history $ip
 	if [ $SECONDS -gt 1 ]
 	then
 		unset ipTable
 		declare -A ipTable
 		sudo iptables -F
 		SECONDS=0
+		echo "Resetting iptables"
 	fi
-	if [ ${ipTable[$line]} -gt 100 ]
-	then
-		continue
-	fi
-	check_history $line
-	echo "IP: $line , No. of sent requests: ${ipTable[$line]}"
-done < <(sudo tcpdump  'icmp[icmptype] == icmp-echo' -tnl | awk '{print $2}')
+#	if [ ${ipTable[$line]} -gt 100 ]
+#	then
+#		continue
+#	fi
+	# echo "IP: $line , No. of sent requests: ${ipTable[$line]}"
+done < <(sudo tcpdump icmp or tcp or udp -tnl)
